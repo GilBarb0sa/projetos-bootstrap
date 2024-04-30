8c4a6a1a64daa6383e85c64f849299492475711b
Gilberto Barbosa da Silva Filho <gilbarbosafilho@gmail.com>
2024-04-29 22:27:14 -0300

    Projeto 1 de bootstrap finalizado

--- projetoAgencia/js/script.js
+++ projetoAgencia/js/script.js
@@ -131,5 +131,41 @@ $(document).ready(function() {  /* instruçao jquery = inicia script somente qua
       });
     }
   }
+
+  // scroll para seções
+  let navBtn = $('.nav-item');
+
+  let bannerSection = $('#mainSlider');
+  let aboutSection = $('#about-area');
+  let servicesSection = $('#services-area');
+  let teamSection = $('#team-area');
+  let portfolioSection = $('#portfolio-area');
+  let contactSection = $('#contact-area');
+
+  let scrollTo = '';
+
+  $(navBtn).click(function() {
+
+    let btnId = $(this).attr('id');
+
+    if(btnId == 'about-menu') {
+      scrollTo = aboutSection;
+    } else if(btnId == 'services-menu') {
+      scrollTo = servicesSection;
+    } else if(btnId == 'team-menu') {
+      scrollTo = teamSection;
+    } else if(btnId == 'portfolio-menu') {
+      scrollTo = portfolioSection;
+    } else if(btnId == 'contact-menu') {
+      scrollTo = contactSection; 
+    } else {
+      scrollTo = bannerSection;
+    }
+  
+    $([document.documentElement, document.body]).animate({  //Animação para subir para o top da página!
+      scrollTop: $(scrollTo).offset().top - 70
+    }, 1000);
+
+  });
 });
 
